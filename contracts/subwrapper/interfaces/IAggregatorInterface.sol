79ae2a676fb9cb0cb92c7158db4bf2e5838d15fa
nxt3d <pm@premmakeig.com>
2023-08-13 23:19:58 -0400

    finish move by deleting files

--- contracts/subwrapper/interfaces/IAggregatorInterface.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-// Chainlink oracle interface
-interface IAggregatorInterface {
-    function latestAnswer() external view returns (int256);
-}
