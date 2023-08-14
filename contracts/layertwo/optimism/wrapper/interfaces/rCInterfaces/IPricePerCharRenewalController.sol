1acde8f5367bf35793573bfed95012bdd13288e6
nxt3d <pm@premmakeig.com>
2023-08-13 23:19:35 -0400

    move file to L2

---
+++ contracts/layertwo/optimism/wrapper/interfaces/rCInterfaces/IPricePerCharRenewalController.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "optimism/wrapper/interfaces/IRenewalController.sol";
+
+/** 
+ * @title An interface for a renewal controller offering renewals at different prices depending on the length of the name in question
+ * @author Unruggable Labs
+ */ 
+
+interface IPricePerCharRenewalController is IRenewalController {
+
+    event CharPricesUpdated(uint256[] indexed charAmounts);
+    event PriceForCharLengthUpdated(uint256 indexed index, uint256 indexed amount);
+
+    function setPricingForAllLengths(
+        uint256[] calldata _charAmounts
+    ) external;
+
+    function updatePriceForCharLength(
+        uint16 charLength,
+        uint256 charAmount
+    ) external;
+
+    function addNextPriceForCharLength(
+        uint256 charAmount
+    ) external;
+
+    function getLastCharIndex() external view returns (uint256);
+}
